d7d1ac908d707b53b3c9aaeaf4ba230c7277ad02
Ameer Nagvenkar <ameer.nagvenkar@sjinnovation.com>
2020-09-05 14:55:01 +0530

    api working

---
+++ amplify/backend/function/lambdaTest01/src/index.js
@@ -0,0 +1,13 @@
+/* Amplify Params - DO NOT EDIT
+	
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    console.log('asdfghjsdfghjsdfghj');
+    return response;
+};
